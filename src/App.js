df946ce8e8fcf51ccfcb792d00a885aadd887833
Anilatta <95362042+Anilatta@users.noreply.github.com>
2022-10-19 17:13:25 +0200

    adding search form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Dictionary from "./Dictionary.js";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>My Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Talina Dzhavakhadze</small>
+        </footer>
+      </div>
     </div>
   );
 }
