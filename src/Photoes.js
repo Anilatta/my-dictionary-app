cd79afe03f1bcad4113bd34770ad6b6e5e7e57cd
Anilatta <95362042+Anilatta@users.noreply.github.com>
2022-10-29 12:06:28 +0200

    Update Photoes.js

--- src/Photoes.js
+++ src/Photoes.js
@@ -5,7 +5,9 @@ export default function Photoes(props) {
     return (
       <section className="Photoes">
         {props.photoes.map(function (photo, index) {
-          return <img src={photo.src.landscape} alt="search result" />;
+          return (
+            <img src={photo.src.landscape} alt="search result" key={index} />
+          );
         })}
       </section>
     );
